3d8f687fe7690db76b1dd6c4fb9c3e7ef6304b9c
Ali Teshnizi <teshnizi@stanford.edu>
2024-09-08 17:28:14 -0700

    serve fe

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 import firebase_admin
 from firebase_admin import credentials, auth
@@ -30,7 +30,7 @@ origins = [
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='../interface/build', template_folder='../interface/build')
     
     # Apply CORS to the Flask app for all routes
     CORS(app, origins=origins, supports_credentials=True)
@@ -78,6 +78,16 @@ def create_app():
     app.register_blueprint(auth.bp, url_prefix="/api/auth")
     app.register_blueprint(projects.bp, url_prefix="/api/projects")
 
+    # Serve React App
+    @app.route('/')
+    def serve():
+        return send_from_directory(app.static_folder, 'index.html')
+
+    @app.route('/<path:path>')
+    def serve_static(path):
+        return send_from_directory(app.static_folder, path)
+
+        
     return app
 
 
