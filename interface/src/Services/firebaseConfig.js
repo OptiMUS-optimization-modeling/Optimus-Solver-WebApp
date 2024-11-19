f11d23b5e4b25713592f75ffdd09b8a001e8716f
Ali Teshnizi <teshnizi@stanford.edu>
2024-11-19 16:55:10 -0500

    myvenv to gitignore

--- interface/src/Services/firebaseConfig.js
+++ interface/src/Services/firebaseConfig.js
@@ -18,6 +18,8 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
+console.log(firebaseConfig);
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
