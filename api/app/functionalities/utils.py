3022337192730472f53e68be46a7753459dc7e40
Ali Teshnizi <teshnizi@stanford.edu>
2024-11-26 21:07:27 -0500

    together api key check raise exception

--- api/app/functionalities/utils.py
+++ api/app/functionalities/utils.py
@@ -6,6 +6,11 @@ def get_llm(model="gpt-4o"):
     if model in ["gpt-4o", "gpt-4o-mini"]:
         return ChatOpenAI(model=model)
     elif model in ["meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo"]:
+        if (
+            not os.environ["TOGETHER_API_KEY"]
+            or len(os.environ["TOGETHER_API_KEY"]) == 0
+        ):
+            raise ValueError("TOGETHER_API_KEY is not set")
         llm = ChatOpenAI(
             base_url="https://api.together.xyz/v1",
             api_key=os.environ["TOGETHER_API_KEY"],
